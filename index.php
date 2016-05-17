16949fb55b912fb1d9ef2d6b07d74c9da33685c3
bpeterson1663 <bpeterson1663@gmail.com>
2016-05-16 22:15:03 -0500

    File Structure and styling finsihed

--- index.php
+++ index.php
@@ -1,82 +1,78 @@
 <?php
-  session_start();
-//checks if submit button was pressed
-  if($_POST['submit']=="Sign Up"){
-    //form validation
-    if(!$_POST['name']){
-      $error.="<br/>Please enter your name";
-    }
-    if(!$_POST['email']){//if they didnt enter anything for email
-      $error.="<br/>Please enter your email";
-    }else if(!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){//validate if valid email
-      $error.="<br/>Please enter a valid email address";
-    }
-    if (!$_POST['password']){//check if they enter anything for a password
-      $error.="<br/>Please enter your password";
-    }else{
-      if(strlen($_POST['password']) < 8 ){//checks if password is atleast 8 characters
-        $error.="<br/>Please enter a password longer than 8 characters.";
-      }
-      if(!preg_match('`[A-Z]`', $_POST['password'])){//checks to see if any capital letters are in the password and if not throw an error
-        $error.="<br/>Please include at least one capital letter in your password";
-      }
-    }
-    if ($error){
-      echo "<br/>There were error(s) in your signup details:".$error;
-    }else{//check to see if email already exists
-      $link = mysqli_connect('localhost', 'root', 'root', 'online_journal');
+include('connection.php');
+include('login.php');
+ ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <script src="vendors/jquery/dist/jquery.min.js" type="text/javascript"></script>
+    <script src="vendors/bootstrap/dist/js/bootstrap.min.js" type="text/javascript"></script>
+    <link href="vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="styles/styles.css" rel="stylesheet" />
+  </head>
+<body data-spy="scroll" data-target=".navbar-collapse">
+  <div class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+      <div class="navbar-header">
+        <button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand">Online Journal</a>
+      </div>
+      <div class="collapse navbar-collapse">
+        <form class="navbar-form navbar-right" method="POST">
+          <div class="form-group">
+            <label for="loginEmail">Email: </label>
+            <input class="form-control" placeholder="Email" type="email" name="loginEmail" id="loginEmail" value="<?php echo addslashes($_POST['email']);?>"/>
+          </div>
+          <div class="form-group">
+            <label for="loginPassword">Password: </label>
+            <input class="form-control" placeholder="Password" type="password" name="loginPassword" value="<?php echo addslashes($_POST['password']);?>"/>
+          </div>
+
+          <input class="btn btn-success" type="submit" name="submit" value="Log In" />
+        </form>
+      </div>
+    </div>
+  </div>
+
+  <div class="container" id="topContainer">
+    <div class="row">
+      <div class="col-md-6 col-md-offset-3">
+        <h1>Online Journal</h1>
+        <p class="lead">Your own personal online journal wherever you want it.</p>
+        <?php
+          if($error){
+            echo '<div class="alert alert-danger">'.addslashes($error).'</div>';
+          }
+          if($message){
+            echo '<div class="alert alert-success">'.addslashes($message).'</div>';
+          }
+         ?>
+        <form method="POST">
+          <div class="form-group">
+            <label for="name">Name</label>
+            <input class="form-control" placeholder="Name" type="name" name="name" id="name" value="<?php echo addslashes($_POST['name']);?>"/>
+          </div>
+          <div class="form-group">
+            <label for="email">Email</label>
+            <input class="form-control" placeholder="Email" type="email" name="email" id="email" value="<?php echo addslashes($_POST['email']);?>"/>
+          </div>
+          <div class="form-group">
+            <label for="password">Password</label>
+            <input class="form-control" placeholder="Password" type="password" name="password" value="<?php echo addslashes($_POST['password']);?>"/>
+          </div>
+
+          <input class="btn btn-lg btn-success" type="submit" name="submit" value="Sign Up" />
+        </form>
 
 
-      $query="SELECT * FROM users WHERE email='".mysqli_real_escape_string($link, $_POST['email'])."'";
-
-      $result = mysqli_query($link, $query);
-
-      $results = mysqli_num_rows($result);
-
-      if($results){
-        echo "That Email address is already registered. Do you want to log in";
-      }else{
-        $query = "INSERT INTO `users` (`name`, `email`, `password`) VALUES( '".$_POST['name']."', ".mysqli_real_escape_string($link, $_POST['email'])."', '".md5(md5($_POST['email']).$_POST['password'])."')";
-
-        mysqli_query($link, $query);
-
-        echo "You've been signed up!";
-
-        $_SESSION['id']=mysqli_insert_id($link);//starts the session with the last person that was created
-      }
-    }
-  }
-
-  if($_POST['submit']=="Log In"){
-
-      $query = "SELECT * FROM users WHERE email='".mysqli_real_escape_string($link, $_POST['loginEmail'])."' AND password='".md5(md5($_POST['loginEmail']).$_POST['loginPassword'])."' LIMIT 1";
-
-      $result = mysqli_query($link, $query);
-
-      $row = mysqli_fetch_array($result);
-
-      if($row){
-        $_SESSION['id']=$row['id'];
-      }else{
-        echo "We Could not find a user with that email and password. Please try again.";
-      }
-  }
-?>
-
-<form method="POST">
-  <label for="name">Name: </label>
-  <input type="name" name="name" id="name" value="<?php echo addslashes($_POST['name']);?>"/>
-  <label for="email">Email: </label>
-  <input type="email" name="email" id="email" value="<?php echo addslashes($_POST['email']);?>"/>
-  <label for="password">Password: </label>
-  <input type="password" name="password" value="<?php echo addslashes($_POST['password']);?>"/>
-  <input type="submit" name="submit" value="Sign Up" />
-</form>
-
-<form method="POST">
-  <label for="loginEmail">Email: </label>
-  <input type="email" name="loginEmail" id="loginEmail" value="<?php echo addslashes($_POST['email']);?>"/>
-  <label for="loginPassword">Password: </label>
-  <input type="password" name="loginPassword" value="<?php echo addslashes($_POST['password']);?>"/>
-  <input type="submit" name="submit" value="Log In" />
-</form>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
---
+++ mainPage.php
@@ -0,0 +1,57 @@
+<?php
+session_start();
+include('connection.php');
+include('login.php');
+
+$query="SELECT entry FROM users WHERE id='".$_SESSION['id']."' LIMIT 1";
+
+$result = mysqli_query($link, $query);
+
+$row = mysqli_fetch_array($result);
+
+$entry = $row['entry'];
+
+ ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Online Journal</title>
+    <script src="vendors/jquery/dist/jquery.min.js" type="text/javascript"></script>
+    <script src="vendors/bootstrap/dist/js/bootstrap.min.js" type="text/javascript"></script>
+    <link href="vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="styles/styles.css" rel="stylesheet" />
+  </head>
+<body data-spy="scroll" data-target=".navbar-collapse">
+  <div class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+      <div class="navbar-header pull-left">
+        <a class="navbar-brand">Online Journal</a>
+      </div>
+      <div class="pull-right">
+        <ul class="navbar-nav nav pull-right">
+          <li><a href="index.php?logout=1">Log Out</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+
+  <div class="container" id="topContainer">
+    <div class="row">
+      <div class="col-md-6 col-md-offset-3">
+        <textarea class="form-control" name="entry">
+
+            <?php echo $entry; ?>
+        </textarea>
+
+      </div>
+    </div>
+  </div>
+  <script>
+  $("textarea").css("height",$(window).height()-120);
+  $("textarea").keyup(function(){
+    $.post("update.php", {entry:$("textarea").val()} );
+  });
+  </script>
+</body>
+</html>
