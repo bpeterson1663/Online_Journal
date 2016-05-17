fd37c8f4934cbb11029dc322a6e1f4db9783e715
bpeterson1663 <bpeterson1663@gmail.com>
2016-05-17 17:28:20 -0500

    DB update

--- connection.php
+++ connection.php
@@ -1,11 +1,3 @@
 <?php
-  //$link = mysqli_connect('localhost', 'root', 'root', 'online_journal');
-$url = parse_url(getenv("heroku_4e7a27171206c39"));
-
-$server = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$db = substr($url["path"], 1);
-
-$link = new mysqli($server, $username, $password, $db);
-?>
+  $link = mysqli_connect('localhost', 'root', 'root', 'online_journal');
+ ?>
