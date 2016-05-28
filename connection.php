3175dd4f17c2fafd816da8c8578b66ca40e69441
bpeterson1663 <bpeterson1663@gmail.com>
2016-05-27 22:33:42 -0500

    db update

--- connection.php
+++ connection.php
@@ -1,7 +1,7 @@
 <?php
   //$url=parse_url("mysql://bf5bc9a564006f:cfd4bb5e@us-cdbr-iron-east-04.cleardb.net/heroku_4e7a27171206c39?reconnect=true");
 
-  $server = 'us-cdbr-iron-east-04.cleardb.net';
+  $server = 'us-cdbr-iron-east-04.cleardb.com';
   $username = 'bf5bc9a564006f';
   $password = 'cfd4bb5e';
   $db = 'heroku_4e7a27171206c39';
--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     session_start();
   }
 
-  $url = parse_url(getenv("mysql://bf5bc9a564006f:cfd4bb5e@us-cdbr-iron-east-04.cleardb.net/heroku_4e7a27171206c39?reconnect=true"));
+  $url = parse_url(getenv("mysql://bf5bc9a564006f:cfd4bb5e@us-cdbr-iron-east-04.cleardb.com/heroku_4e7a27171206c39?reconnect=true"));
 
   $server = $url["host"];
   $username = $url["user"];
