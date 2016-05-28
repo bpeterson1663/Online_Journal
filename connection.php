2f08cbf997fd3c8db63fd1c9b78279678fe692db
bpeterson1663 <bpeterson1663@gmail.com>
2016-05-27 21:08:05 -0500

    DB update

--- connection.php
+++ connection.php
@@ -1,12 +1,12 @@
 <?php
-  $url=parse_url(getenv("CLEARDB_DATABASE_URL"));
+  $url=parse_url(getenv("mysql://bf5bc9a564006f:cfd4bb5e@us-cdbr-iron-east-04.cleardb.net/heroku_4e7a27171206c39?reconnect=true"));
 
   $server = $url["host"];
   $username = $url["user"];
   $password = $url["pass"];
   $db = substr($url["path"],1);
 
-  $link = mysql_connect($server, $username, $password);
+  $link = mysqli_connect($server, $username, $password, $db);
 
-  mysql_select_db($db);
+  //mysql_select_db($db);
  ?>
