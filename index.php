daa1641b96082db35a53840b7f693408554e8262
bpeterson1663 <bpeterson1663@gmail.com>
2016-05-27 21:55:04 -0500

    Db test

--- index.php
+++ index.php
@@ -10,12 +10,10 @@
     $message = "You have been logged out. Have a nice day!";
     session_start();
   }
-  $url=parse_url(getenv("mysql://bf5bc9a564006f:cfd4bb5e@us-cdbr-iron-east-04.cleardb.net/heroku_4e7a27171206c39?reconnect=true"));
-
-  $server = $url["host"];
-  $username = $url["user"];
-  $password = $url["pass"];
-  $db = substr($url["path"],1);
+  $server = 'us-cdbr-iron-east-04.cleardb.net';
+  $username = 'bf5bc9a564006f';
+  $password = 'cfd4bb5e';
+  $db = 'heroku_4e7a27171206c39';
 
   $link = mysqli_connect($server, $username, $password, $db);
 
--- mainPage.php
+++ mainPage.php
@@ -1,14 +1,12 @@
 <?php
 	session_start();
 
-	$url=parse_url(getenv("mysql://bf5bc9a564006f:cfd4bb5e@us-cdbr-iron-east-04.cleardb.net/heroku_4e7a27171206c39?reconnect=true"));
+	$server = 'us-cdbr-iron-east-04.cleardb.net';
+	$username = 'bf5bc9a564006f';
+	$password = 'cfd4bb5e';
+	$db = 'heroku_4e7a27171206c39';
 
-  $server = $url["host"];
-  $username = $url["user"];
-  $password = $url["pass"];
-  $db = substr($url["path"],1);
-
-  $link = mysqli_connect($server, $username, $password, $db);
+	$link = mysqli_connect($server, $username, $password, $db);
 
 	$query="SELECT entry FROM users WHERE id='".$_SESSION['id']."' LIMIT 1;";
 
