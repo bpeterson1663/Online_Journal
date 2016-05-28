08461d038ce83d2f192cd1c38b609f0d97f83d21
bpeterson1663 <bpeterson1663@gmail.com>
2016-05-27 21:41:28 -0500

    include does not seem to work

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 
 <?php
+
+  $url=parse_url(getenv("mysql://bf5bc9a564006f:cfd4bb5e@us-cdbr-iron-east-04.cleardb.net/heroku_4e7a27171206c39?reconnect=true"));
+
+  $server = $url["host"];
+  $username = $url["user"];
+  $password = $url["pass"];
+  $db = substr($url["path"],1);
+
+  $link = mysqli_connect($server, $username, $password, $db);
+
   session_start();
 
   if($_GET['logout']==1 AND $_SESSION['id']){
