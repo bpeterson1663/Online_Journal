85bd012c42717740b287ec86eeb85d7eb1bfa394
bpeterson1663 <bpeterson1663@gmail.com>
2016-05-28 22:28:56 -0500

    Updated the update.php function

--- update.php
+++ update.php
@@ -1,7 +1,12 @@
 <?php
 
   session_start();
-  include('connection.php');
+  $server = 'us-cdbr-iron-east-04.cleardb.net';
+  $username = 'b8c9d44004cc5d';
+  $password = '42f42c8f';
+  $db = 'heroku_7da4bb181d6ffac';
+
+  $link = mysqli_connect($server, $username, $password, $db);
 
   $query = "UPDATE users SET entry='".mysqli_real_escape_string($link, $_POST['entry'])."' WHERE id='".$_SESSION['id']."' LIMIT 1";
 
